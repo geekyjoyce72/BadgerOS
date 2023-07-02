bbfb45fb95f4461ba98071852beafef670ad58b6
RobotMan2412 <julian@scheffers.net>
2023-07-02 13:46:17 +0200

    Add an interrupt enabler.

--- port/esp32c6/include/port/interrupt.h
+++ port/esp32c6/include/port/interrupt.h
@@ -17,3 +17,14 @@
 // Install interrupt and trap handlers.
 // Requires a preallocated context and regs struct.
 void interrupt_init(kernel_ctx_t *ctx);
+
+// Disable interrupts and return whether they were enabled.
+static inline bool interrupt_disable() {
+	uint32_t mstatus;
+	asm volatile("csrrc %0, mstatus, %1" : "=r" (mstatus) : "r" (8));
+	return mstatus & 8;
+}
+// Enable interrupts.
+static inline void interrupt_enable() {
+	asm volatile("csrs mstatus, %0" :: "r" (8));
+}
