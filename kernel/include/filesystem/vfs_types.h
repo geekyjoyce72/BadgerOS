d22743ae14c4377a218e3292b2bcdb4274ad0d24
RobotMan2412 <julian@scheffers.net>
2025-01-03 01:04:53 +0100

    OOPS 2.0
    This reverts commit e192bb6aabe0e581b50d46e4fd131be828dc21a3.
    
    .. and then does what I wanted to do in the first place

--- kernel/include/filesystem/vfs_types.h
+++ kernel/include/filesystem/vfs_types.h
@@ -3,56 +3,59 @@
 
 #pragma once
 
-// VFS shared opened file handle.
-// Shared between all file handles referring to the same file.
-typedef struct vfs_file_obj  vfs_file_obj_t;
-// VFS opened file handle.
-typedef struct vfs_file_desc vfs_file_desc_t;
-// VFS mounted filesystem.
-typedef struct vfs           vfs_t;
-
 #include "blockdevice.h"
 #include "filesystem.h"
-#include "filesystem/vfs_vtable.h"
+#include "filesystem/vfs_fat_types.h"
+#include "filesystem/vfs_ramfs_types.h"
 #include "mutex.h"
 
+typedef struct vfs vfs_t;
 
 // VFS shared opened file handle.
 // Shared between all file handles referring to the same file.
-struct vfs_file_obj {
-    // Reference count (how many `vfs_file_desc_t` reference this).
-    atomic_int refcount;
+typedef struct {
+    // Reference count.
+    size_t    refcount;
     // Index in the shared file handle table.
-    ptrdiff_t  index;
+    ptrdiff_t index;
     // Current file size.
-    fileoff_t  size;
+    fileoff_t size;
     // Filesystem-specific information.
-    void      *cookie;
-    // Type of file this is.
-    filetype_t type;
+    union {
+        // RAMFS.
+        vfs_ramfs_file_t ramfs_file;
+        // FAT12, FAT16 or FAT32.
+        vfs_fat_file_t   fat_file;
+    };
+
+    // Cached region offset.
+    fileoff_t cache_off;
+    // Cached region size.
+    fileoff_t cache_size;
+    // Cached register buffer.
+    char     *cache;
+
     // Inode number (gauranteed to be unique per VFS).
     // No file or directory may have the same inode number.
-    // Any file or directory is required to name an inode number of 1 or higher.
-    inode_t    inode;
+    // Any file is required to name an inode number of 3 or higher.
+    inode_t inode;
     // Pointer to the VFS on which this file exists.
-    vfs_t     *vfs;
-    // Handle mutex for concurrency.
-    mutex_t    mutex;
-};
+    vfs_t  *vfs;
+} vfs_file_shared_t;
 
 // VFS opened file handle.
-struct vfs_file_desc {
-    // Reference count (how many threads reference this).
-    atomic_int refcount;
+typedef struct {
     // Current access position.
     // Note: Must be bounds-checked on every file I/O.
-    fileoff_t  offset;
+    fileoff_t offset;
     // File is writeable.
-    bool       write;
+    bool      write;
     // File is readable.
-    bool       read;
+    bool      read;
     // Handle refers to a directory.
-    bool       is_dir;
+    bool      is_dir;
+    // Handle mutex for concurrency.
+    mutex_t   mutex;
 
     // Directories: Cached size.
     fileoff_t dir_cache_size;
@@ -61,40 +64,28 @@ struct vfs_file_desc {
 
     // Pointer to shared file handle.
     // Directories do not have a shared handle.
-    vfs_file_obj_t *obj;
+    vfs_file_shared_t *shared;
     // Handle number.
-    file_t          fileno;
-};
+    file_t             fileno;
+} vfs_file_handle_t;
 
 // VFS mounted filesystem.
 struct vfs {
-    // Filesystem vtable.
-    vfs_vtable_t const *vtable;
     // Copy of mount point.
-    char               *mountpoint;
+    char     *mountpoint;
     // Read-only flag.
-    bool                readonly;
+    bool      readonly;
     // Associated block device.
-    blkdev_t           *media;
+    blkdev_t *media;
     // Filesystem type.
-    fs_driver_t const  *driver;
+    fs_type_t type;
     // Inode number given to the root directory.
-    inode_t             inode_root;
+    inode_t   inode_root;
     // Filesystem-specific information.
-    void               *cookie;
+    union {
+        // RAMFS.
+        vfs_ramfs_t ramfs;
+        // FAT12, FAT16 or FAT32.
+        vfs_fat_t   fat;
+    };
 };
-
-// Filesystem implementation info.
-typedef struct {
-    // Filesystem ID.
-    char const         *id;
-    // Filesystem vtable.
-    vfs_vtable_t const *vtable;
-    // Size to allocate for the VFS cookie.
-    size_t              vfs_cookie_size;
-    // Size to allocate for the file handle cookie.
-    size_t              file_cookie_size;
-} fs_driver_t;
-
-// Declare a filesystem driver.
-#define FS_DRIVER_DECL(ident) __attribute__((section(".fsdrivers"))) static fs_driver_t const ident
